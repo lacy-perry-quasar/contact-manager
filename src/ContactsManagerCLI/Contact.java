ecc31ab1b26ebc8f1814c6dc39faaa1c18549337
Rodriques <rodriques.perry@gmail.com>
2021-11-12 14:02:44 -0600

    add ContactsManager class with addContact and showContacts methods

--- src/ContactsManagerCLI/Contact.java
+++ src/ContactsManagerCLI/Contact.java
@@ -1,11 +1,15 @@
 package ContactsManagerCLI;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Contact {
 
 	protected String firstName;
 	protected String lastName;
 	protected String phoneNumber;
-
 
 	public Contact(String firstName, String lastName, String phoneNumber) {
 
@@ -14,6 +18,20 @@ public class Contact {
 		this.phoneNumber = phoneNumber;
 	}
 
-	public String getName() {return firstName + lastName;}
+	public String getName() {return firstName + " " + lastName;}
+
 	public String getPhoneNumber() {return phoneNumber;}
+
+
+	public static void main(String[] args) {
+		Contact steve = new Contact("Steven", "Lacy", "222-222-2222");
+		Contact rod = new Contact("Rodriques", "Perry", "333-333-3333");
+		Contact ken = new Contact("Kenneth", "Howell", "444-444-4444");
+
+		System.out.println("steve.getName() = " + steve.getName());
+		System.out.println("rod.getName() = " + rod.getName());
+		System.out.println("ken.getName() = " + ken.getName());
+
+	}
+
 }
--- src/ContactsManagerCLI/Main.java
+++ src/ContactsManagerCLI/Main.java
@@ -1,71 +1,15 @@
 package ContactsManagerCLI;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		boolean done = false;
-		int selection = 0;
-		boolean invalidChoice = false;
-		Scanner keyboard = new Scanner(System.in);
+		ContactsManager.showMenu();
 
-		while (!done) {
-			// print out menu for the user
-			// to select from
-			System.out.println("\nPlease select what you would like to do \n");
-			System.out.println("1 - View Contacts.");
-			System.out.println("2 -  Add a new contact.");
-			System.out.println("3 - Search a contact by name.");
-			System.out.println("4 - Delete an existing contact.");
-			System.out.println("5 - exit\n");
 
-			// read in from the keyboard what
-			// the user has entered
-			selection = keyboard.nextInt();
-
-			// using nested if statements, determine what
-			// operation the user has selected (add, subtract,
-			// multiply, etc.)
-			if (selection == 1) //view contacts
-			{
-
-				invalidChoice = false;
-			} else if (selection == 2) //subtract
-			{
-				invalidChoice = false;
-			} else if (selection == 3) //mulitply
-			{
-				answer1 = number1 * number2;
-				invalidChoice = false;
-			} else if (selection == 4) //integar division
-			{
-				answer1 = number1 / number2;
-				invalidChoice = false;
-			} else if (selection == 5) //exit program
-			{
-				done = true;
-				invalidChoice = false;
-			} else    //invalid choice
-			{
-				invalidChoice = true;
-				System.out.println("\n***ERROR*** " +
-						selection +
-						"is not a valid selection. " +
-						"Please try again!");
-			}
-
-			// using nested if statements, determine
-			// the type of output to produce
-			if (selection == 5 && invalidChoice == false) {
-				System.out.println("\nCalculated answer is: " +
-						answer2);
-			} else if (selection != 6 && invalidChoice == false) {
-				System.out.println("\nCalculated answer is: " +
-						answer1);
-			} else {
-				System.out.println("\nNo calculation done.");
-			}
-		}    //end wile loop
 	}
+
+
 }
